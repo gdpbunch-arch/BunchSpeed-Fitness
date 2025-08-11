083c05eb1639398ed85759f393c33f4070da6529
Shawn Bunch <bunchspeednutrition@kevibllc.com>
2025-08-10 19:57:24 -0700

    Add service worker

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,36 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', () => {}); // minimal handler to satisfy installability
+const CACHE = 'bsn-v1';
+const ASSETS = [
+  '/',
+  '/manifest.webmanifest',
+  '/brand/icon-192.png',
+  '/brand/icon-512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+      return fetch(req).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((cache) => cache.put(req, copy));
+        return res;
+      });
+    })
+  );
+});
